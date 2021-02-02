a9d58945e7dd2b6f02b8958c7ded3380a19abdf5
GianRondo91 <giancarlorondo@outlook.com>
2021-02-02 21:35:06 +0100

    Estructura del proyecto

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let cambiaPantalla = (faseAhora, faseFutura) => {
+    let pantallaActual = document.getElementById(pantallaDos);
+
+    let pantallaDestino = document.getElementById(pantallaUno);
+
+    //aqui procedemos con el cambio
+
+    pantallaActual.style.display = "none";
+    pantallaDestino.style.display = "block";
+};
+
+//Cambiar de pantalla porque ya tenemos a los personajes elegidos
+
+resolveIn(1000).then(delay => {
+
+    cambiaPantalla("screen1", "screen2");
+
+});
+
+//funcion de delay...
+
+const resolveIn = delay =>
+    new Promise(res => setTimeout(() => res(delay), delay));
